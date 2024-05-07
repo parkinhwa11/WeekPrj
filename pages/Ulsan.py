580929771b1546a3f65d7a604e5a91eb69200da1
Inhwa <qkrdlsghk02@gmail.com>
2024-05-07 16:31:01 +0900

    git add files

--- pages/Ulsan.py
+++ pages/Ulsan.py
@@ -35,7 +35,7 @@ def tabs(tabnum, name, googlelink, intro, image1, image2, data, pos_cnt, neg_cnt
 
         with col1:
             st.markdown('**Image**')
-            st.image(Image.open(image1),
+            st.image(image1,
                      use_column_width=True)
 
         with col2:
@@ -44,7 +44,7 @@ def tabs(tabnum, name, googlelink, intro, image1, image2, data, pos_cnt, neg_cnt
             row2 = st.columns(2)
             for i, col in enumerate(row1 + row2):
                 tile = col.expander(rec_place[i])
-                tile.image(Image.open(rec_place_img[i]),
+                tile.image(rec_place_img[i],
                      caption=rec_caption[i],
                      use_column_width=True)
 
@@ -55,7 +55,7 @@ def tabs(tabnum, name, googlelink, intro, image1, image2, data, pos_cnt, neg_cnt
         with col1:
             st.markdown('💡**Highlights of the Destination**')
             st.text('(Top Keywords based on Korean blog)')
-            st.image(Image.open(image2),
+            st.image(image2,
                      use_column_width=True)
         with col2:
             data1 = pd.read_csv(data)
@@ -103,7 +103,7 @@ def tabs(tabnum, name, googlelink, intro, image1, image2, data, pos_cnt, neg_cnt
         st.subheader(f'**:green[{positive_display}]** **:red[{negative_display}]**')
 
         with st.expander('Review text positive/negative word distribution (Bigram NetworkX Graph)'):
-            st.image(Image.open(image3), use_column_width=True)
+            st.image(image3, use_column_width=True)
 
 # 간월재--------------------------------------------------------------------------------------
 
